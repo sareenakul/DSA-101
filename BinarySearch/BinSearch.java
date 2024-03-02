6a4cc7ac0afcc6a9d72d417eebff4b951f3ee280
Akul Sareen <sareen.akul@gmail.com>
2024-03-01 23:51:55 -0500

    Recursive BinarySearch

--- BinarySearch/BinSearch.java
+++ BinarySearch/BinSearch.java
@@ -1,5 +1,6 @@
 package BinarySearch;
 public class BinSearch {
+    // Normal Binary Search with Time Complexity O(log(n))
     public static int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
@@ -19,10 +20,29 @@ public class BinSearch {
         return -1;
     }
 
+    public static int recursiveBS(int[] nums, int target, int start, int end){
+        if(end >= start){
+           int mid = (end - start)/2 + start;
+        
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                return recursiveBS(nums, target, start, mid - 1);
+            }
+            else if(nums[mid] < target){
+                return recursiveBS(nums, target, mid + 1, end);
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] list = {1,2,3,4,5,6,7,8,9,10};
         int target = 6;
-        int index = search(list, target);
-        System.out.println(index);
+        int index1 = search(list, target);
+        int index2 = recursiveBS(list, target, 0, list.length - 1);
+        System.out.println(index1);
+        System.out.println(index2);
     }
 }
