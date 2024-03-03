dd64ce918d4f46bed3859a17a4e2277e0cd2e47a
Akul Sareen <sareen.akul@gmail.com>
2024-03-02 21:24:31 -0500

    Search Range of a target using Binary Search

---
+++ BinarySearch/SearchRange.java
@@ -0,0 +1,60 @@
+package BinarySearch;
+
+public class SearchRange {
+    public static int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = firstOccurance(nums, target);
+        result[1] = lastOccurance(nums, target);
+        return result;
+    }
+    public static int firstOccurance(int[] nums, int target){
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = 0;
+        int index = -1;
+        while(end >= start){
+            mid = (end - start)/2 + start;
+            if(nums[mid] == target){
+                index = mid;
+                end = mid - 1;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return index;
+    }
+
+    public static int lastOccurance(int[] nums, int target){
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = 0;
+        int index = -1;
+        while(end >= start){
+            mid = (end - start)/2 + start;
+            if(nums[mid] == target){
+                index = mid;
+                start = mid + 1;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {1,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,4,4,5,5,6,6,7};
+        int target = 1;
+        int[] result = searchRange(list, target);
+        for(int num : result){
+            System.out.println(num);
+        }
+    }
+}
