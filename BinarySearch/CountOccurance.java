b6062d1669d6ab0b839983997921d908c7e2c632
Akul Sareen <sareen.akul@gmail.com>
2024-03-02 21:46:30 -0500

    GFG countOccurance

---
+++ BinarySearch/CountOccurance.java
@@ -0,0 +1,63 @@
+package BinarySearch;
+
+public class CountOccurance {
+    public static int count(int[] arr, int n, int x) {
+        int last = lastOccurance(arr, x);
+        int first = firstOccurance(arr, x);
+        if(first == -1){
+            return 0;
+        }
+        return(last - first + 1);
+    }
+    
+    public static int firstOccurance(int[] nums, int target){
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = 0;
+        int index = -1;
+        while(end >= start){
+            mid = (end - start)/2 + start;
+            if(nums[mid] == target){
+                index = mid;
+                end = mid - 1;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return index;
+    }
+    
+    public static int lastOccurance(int[] nums, int target){
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = 0;
+        int index = -1;
+        while(end >= start){
+            mid = (end - start)/2 + start;
+            if(nums[mid] == target){
+                index = mid;
+                start = mid + 1;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {1,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,4,4,5,5,6,6,7};
+        int target = 1;
+        int result = count(list, list.length, target);
+        System.out.println(result);
+    }
+}
+
+// https://www.geeksforgeeks.org/problems/number-of-occurrence2259/1
